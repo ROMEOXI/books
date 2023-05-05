84f67a9377702ab7bed15d2a60e741ed9bc0f7f0
WangXi <jason942371@gmail.com>
2023-05-05 18:32:47 +0800

    Deleting Records

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  return (
-    <div>
-      App
-    </div>
-  );
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  }
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title }
+    ];
+
+    setBooks(updatedBooks);
+  };
+
+  return <div className="App">
+    <BookList books={books} onDelete={deleteBookById} />
+    <BookCreate onCreate={createBook} />
+  </div>;
 }
 
 export default App;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,11 @@
-function BookList() {
-    return ( <div>BookList</div> );
+import BookShow from './BookShow';
+
+function BookList({ books, onDelete }) {
+    const rerenderedBooks = books.map((book) => {
+        return <BookShow onDelete={onDelete} keey={book.id} book={book} />;
+    });
+
+    return (<div className='book-list'>{rerenderedBooks}</div>);
 }
 
 export default BookList;
