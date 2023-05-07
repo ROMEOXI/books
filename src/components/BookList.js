d855c12ee07d625fa640fd0b5c791fbb48684740
WangXi <jason942371@gmail.com>
2023-05-07 16:46:01 +0800

    Added the ability to update titles

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,8 @@
 import BookShow from './BookShow';
 
-function BookList({ books, onDelete }) {
+function BookList({ books, onDelete, onEdit }) {
     const rerenderedBooks = books.map((book) => {
-        return <BookShow onDelete={onDelete} keey={book.id} book={book} />;
+        return <BookShow onEdit={onEdit} onDelete={onDelete} keey={book.id} book={book} />;
     });
 
     return (<div className='book-list'>{rerenderedBooks}</div>);
