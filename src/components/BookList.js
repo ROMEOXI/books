0fe61035a983b088a7125cd47478dce72eaf99e2
WangXi <jason942371@gmail.com>
2023-05-08 11:54:03 +0800

    feat image get

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -2,7 +2,7 @@ import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
     const rerenderedBooks = books.map((book) => {
-        return <BookShow onEdit={onEdit} onDelete={onDelete} keey={book.id} book={book} />;
+        return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
     });
 
     return (<div className='book-list'>{rerenderedBooks}</div>);
