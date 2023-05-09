cc61eeb103f175543b27ec9c86cbf3b753621dc4
WangXi <jason942371@gmail.com>
2023-05-09 18:29:34 +0800

    refactor: Refactor to context version

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,15 @@
-import BookShow from './BookShow';
+import { useContext } from "react";
+import BooksContext from "../context/books";
+import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
-    const rerenderedBooks = books.map((book) => {
-        return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
-    });
+  const rerenderedBooks = books.map((book) => {
+    return (
+      <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
+    );
+  });
 
-    return (<div className='book-list'>{rerenderedBooks}</div>);
+  return <div className="book-list">{rerenderedBooks}</div>;
 }
 
 export default BookList;
