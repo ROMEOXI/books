84b91c56938d070aa733e69a3429bf9adafd9cd1
WangXi <jason942371@gmail.com>
2023-05-10 14:52:45 +0800

    refactor: All compeleted

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,26 +1,30 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
 
-function BookCreate({ onCreate }) {
-    const [title, setTitle] = useState('');
+function BookCreate() {
+  const [title, setTitle] = useState("");
+  const { createBook } = useContext(BooksContext);
 
-    const handleChange = (event) => {
-        setTitle(event.target.value);
-    };
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
 
-    const handleSumbit = (event) => {
-        event.preventDefault();
-        onCreate(title);
-        setTitle('');
-    };
+  const handleSumbit = (event) => {
+    event.preventDefault();
+    createBook(title);
+    setTitle("");
+  };
 
-    return <div className="book-create">
-        <h3>Add a book</h3>
-        <form onSubmit={handleSumbit} >
-            <label>Title</label>
-            <input className="input" value={title} onChange={handleChange} />
-            <button className="button">Create!</button>
-        </form>
-    </div>;
+  return (
+    <div className="book-create">
+      <h3>Add a book</h3>
+      <form onSubmit={handleSumbit}>
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create!</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
